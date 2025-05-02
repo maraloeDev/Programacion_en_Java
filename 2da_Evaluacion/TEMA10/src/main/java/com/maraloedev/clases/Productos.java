9d232c34aee2cfe3e430ffbe093f385339384050
Eduardo <martinsonsecaeduardo@gmail.com>
2025-05-02 16:45:32 +0200

    ACTUALIZACION

---
+++ 2da_Evaluacion/TEMA10/src/main/java/com/maraloedev/clases/Productos.java
@@ -0,0 +1,69 @@
+package com.maraloedev.clases;
+
+
+public class Productos {
+    
+    private int idProducto;
+    private String descripcion;
+    private Double precio;
+    private int cantidad;
+    private int tipo;
+
+    public Productos(int idProducto, String descripcion, Double precio, int cantidad, int tipo) {
+        this.idProducto = idProducto;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.tipo = tipo;
+    }
+
+    public Productos() {
+        
+        
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    
+    @Override 
+    public String toString(){ 
+        return idProducto + ": " + descripcion + " --> " + precio + " €"; 
+    }
+}
---
+++ 2da_Evaluacion/TEMA10/src/main/java/com/maraloedev/clases/TiposProducto.java
@@ -0,0 +1,34 @@
+package com.maraloedev.clases;
+
+
+public class TiposProducto {
+    
+    private int idTipo;
+    private Character tipo;
+
+    public TiposProducto(int idTipo, Character tipo) {
+        this.idTipo = idTipo;
+        this.tipo = tipo;
+    }
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
+
+    public Character getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Character tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(tipo);
+    }
+}
